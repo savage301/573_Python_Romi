e187bab9d142c02ed9396c4c3ebd5db7f1f0617f
Eric Savage <savage301@gmail.com>
2024-01-22 14:01:15 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+#To Launch run py -3 robotpy sim --ws-client
 
 
-
-sensor_threshold_value = 175
+sensor_threshold_value = 175 #Once the value goes under this the auto rotuine will stop
 
 ip_address = "10.5.73.1"
+
+
