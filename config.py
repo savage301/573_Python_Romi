0d7c7c38a0b34d6178c713cac6c4716f85dd55f3
Eric Savage <savage301@gmail.com>
2024-01-28 13:11:02 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-#To Launch run py -3 -m robotpy sim --ws-client
+#To run code, enter "py -3 -m robotpy sim --ws-client" into the terminal
 
-sensor_gt_value = False # This controls if autonomous stops when value is above threshold or below threshold
+sensor_gt_value = False #This controls if autonomous stops when value is above threshold (True) or below threshold (False)
 
 sensor_threshold_value = 175 #Once the value goes under this the auto rotuine will stop
 
-ip_address = "10.5.73.1"
+ip_address = "10.5.73.1" #Change the "1" at the end of this IP address to match your Romi number
 
 
 
